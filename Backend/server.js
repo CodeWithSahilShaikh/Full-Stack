9180d5c34b7470e5c2dd26d3ac62592064ff4a43
Sahil Shaikh <sahil58045@gmail.com>
2026-02-22 01:46:08 +0530

    note-to-remeber

--- Backend/server.js
+++ Backend/server.js
@@ -17,3 +17,4 @@ app.listen(3000, () => {
 // but yaha ek dikkt aa rhi hai ki pehle hm localhost 3000 pr backend run ho rha tha and ab use ek url mil gya hai "https://full-stack-1-o577.onrender.com/" jo ki cloude pr run ho rha hai but jo hmari apis thi get post delete wali wo abhi bhi localhost pr hai. toh hm unn apis me localhost ki jgh render ki url dalenge.
 // example: http://localhost:3000/api/notes  [ye localhost ki hai]
 // ise hm change kr denge: https://full-stack-1-o577.onrender.com/api/notes
+// toh frontend ke app.jsx me jaha jaha bhi localhost api fetch kr rhe hai wha wha isse replace kr denge.
